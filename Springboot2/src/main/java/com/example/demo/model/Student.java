8cc9cde11da8cea92790dd71224a5d6a3cb8b0d9
Dinuki85 <prarthanadinuki2@gmail.com>
2025-03-30 15:52:13 +0530

    Feat:Generate getter and setter methods

--- Springboot2/src/main/java/com/example/demo/model/Student.java
+++ Springboot2/src/main/java/com/example/demo/model/Student.java
@@ -14,6 +14,38 @@ public Student(String regNo, String name, int age, String course, double gpa) {
 	this.course = course;
 	this.gpa = gpa;
 }
+public String getRegNo() {
+	return regNo;
+}
+public void setRegNo(String regNo) {
+	this.regNo = regNo;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public int getAge() {
+	return age;
+}
+public void setAge(int age) {
+	this.age = age;
+}
+public String getCourse() {
+	return course;
+}
+public void setCourse(String course) {
+	this.course = course;
+}
+public double getGpa() {
+	return gpa;
+}
+public void setGpa(double gpa) {
+	this.gpa = gpa;
+}
+
+
  
  
 }
